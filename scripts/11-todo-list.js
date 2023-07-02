d38ce414e1f90f69b64988eae22d39e9ad2b0523
Entoma-Von-Degurechaff <68795418+Entoma-Von-Degurechaff@users.noreply.github.com>
2023-07-02 19:05:04 +0900

    3 files modified

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,10 +1,27 @@
-  const todoList = [];
+  const todoList = ['make dishes', 'laundry'];
+
+  renderTodoList();
+
+  function renderTodoList() {
+    let todoListHTML = '';
+    for(let i = 0; i < todoList.length; i++) {
+      const todo = todoList[i];
+      const htmlTodo = `<p>${todo}</p>`
+      todoListHTML += htmlTodo;
+    }
+  }
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
   function addTodo() {
     const inputElement = document.querySelector('.name-of-todo');
     const name = inputElement.value;
-    // console.log(name);
+  
     todoList.push(name);
+
+
     console.log(todoList);
     inputElement.value = '';
+
+    renderTodoList();
   }
