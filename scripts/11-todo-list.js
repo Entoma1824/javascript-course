2a4afb70487d4d6ce89b8e84a700e6692b10da36
Entoma-Von-Degurechaff <68795418+Entoma-Von-Degurechaff@users.noreply.github.com>
2023-07-03 19:24:21 +0900

    2 files modified, 1 file added

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,27 +1,47 @@
-  const todoList = ['make dishes', 'laundry'];
+const todoList = [{
+  name: 'make dinner',
+  dueDate: '2022-12-22'
+}, {
+  name: 'wash dishes',
+  dueDate: '2022-12-22'
+}];
+
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    const { name, dueDate } = todoObject;
+
+
+    const html = `
+    <div>${name}</div>
+    <div>${dueDate}</div>
+    <button onclick="
+        todoList.splice(${i}, 1);
+        renderTodoList();
+      " class="delete-todo-button">Delete</button>`;
+    todoListHTML += html;
+  }
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
+
+function addTodo() {
+  const inputElement = document.querySelector('.js-name-input');
+  const name = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    name,
+    dueDate
+  });
+
+  inputElement.value = '';
 
   renderTodoList();
-
-  function renderTodoList() {
-    let todoListHTML = '';
-    for(let i = 0; i < todoList.length; i++) {
-      const todo = todoList[i];
-      const htmlTodo = `<p>${todo}</p>`
-      todoListHTML += htmlTodo;
-    }
-  }
-
-  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
-
-  function addTodo() {
-    const inputElement = document.querySelector('.name-of-todo');
-    const name = inputElement.value;
-  
-    todoList.push(name);
-
-
-    console.log(todoList);
-    inputElement.value = '';
-
-    renderTodoList();
-  }
+}
