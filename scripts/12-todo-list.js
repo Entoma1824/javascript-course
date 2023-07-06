da32fa6f9d4afadd6cd0e626ccb0f6c8819e9072
Entoma-Von-Degurechaff <68795418+Entoma-Von-Degurechaff@users.noreply.github.com>
2023-07-06 15:12:45 +0900

    3 files modified

--- scripts/12-todo-list.js
+++ scripts/12-todo-list.js
@@ -12,32 +12,29 @@ function renderTodoList() {
   let todoListHTML = '';
 
   todoList.forEach((todoObject, index) => {
-    todoObject = todoList[index];
     const { name, dueDate } = todoObject;
-
     const html = `
-    <div>${name}</div>
-    <div>${dueDate}</div>
-    <button class="delete-todo-button js-delete-todo-button">Delete</button>`;
+      <div>${name}</div>
+      <div>${dueDate}</div>
+      <button class="delete-todo-button js-delete-todo-button">Delete</button> 
+    `;
     todoListHTML += html;
   });
+
   document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 
-  document.querySelectorAll('.js-delete-todo-button').addEventListener('click', () => {
-    todoList.splice(index, 1);
-    renderTodoList();
-  })
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      console.log(index)
+      todoList.splice(index,1);
+      renderTodoList();
+    });
+  });
 }
 
-document.querySelector('.js-add-button').forEach((deleteButton, index) => {
-  deleteButton.addEventListener('click', () => {
-    todoList.splice(index, 1);
-    renderTodoList();
-  })
-}
-
-
-) 
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+})
 
 function addTodo() {
   const inputElement = document.querySelector('.js-name-input');
